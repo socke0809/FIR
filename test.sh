c36abbb883c20f593a2d88e835f2216b76acf679
x5444 <x5444@x5444.com>
2013-11-27 07:04:19 +0100

    Added a proper makefile.

--- test.sh
+++ test.sh
@@ -1,5 +1,4 @@
 #! /bin/bash
 
-gcc -std=c99 -o fir main.c -lm
-./fir > output.dat
+./main.elf > output.dat
 gnuplot -p -e "plot 'output.dat' using 1:3 title 'Output' with lines, 'output.dat' using 1:2 title 'Input' with lines"
